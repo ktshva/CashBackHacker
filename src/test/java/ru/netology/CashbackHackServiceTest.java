5807ea2b645f6745d73b7a6ba320aaff5959c048
Arina Kotysheva <kotysheva.arina@gmail.com>
2024-02-28 21:39:05 +0700

    pre push testng

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void ShouldExtra100When900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldNotExtraIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldExtra499When1501() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1501;
+        int actual = service.remain(amount);
+        int expected = 499;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldExtra1000When0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+}
